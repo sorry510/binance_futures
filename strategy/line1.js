22bd27b90895937380e9080d8a35c62de583b71a
HaoZhengZhao <haozheng.zhao@poper.co>
2023-06-15 17:09:39 +0800

    fix: 调整 line1

--- strategy/line1.js
+++ strategy/line1.js
@@ -54,11 +54,11 @@ async function getLongOrShort(symbol) {
 
   const ma1 = await binance.getKline(symbol, '1m', 5) // 1min的kline 最近 n 条值
   const ma2 = await binance.getKline(symbol, '3m', 5) // 3min的kline 最近 n 条值
-  if (isDesc(ma1.slice(0, 5)) && isDesc(ma2.slice(0, 4)) && kdj(ma1.slice(0, 2), ma2.slice(0, 2), 'long')) { // 产生了金叉
+  if (isDesc(ma1.slice(0, 5)) && isDesc(ma2.slice(0, 3)) && kdj(ma1.slice(0, 2), ma2.slice(0, 2), 'long')) { // 产生了金叉
     // 涨的时刻
     canLong = true
     canShort = false
-  } else if (isAsc(ma1.slice(0, 5)) && isAsc(ma2.slice(0, 4)) && kdj(ma1.slice(0, 2), ma2.slice(0, 2), 'short')) {
+  } else if (isAsc(ma1.slice(0, 5)) && isAsc(ma2.slice(0, 3)) && kdj(ma1.slice(0, 2), ma2.slice(0, 2), 'short')) {
     // 跌的时刻
     canLong = false
     canShort = true
