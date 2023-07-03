58735251d929c254a96de1ba7eedfdcff9a1c08b
HaoZhengZhao <haozheng.zhao@poper.co>
2023-07-03 15:15:06 +0800

    fix: 调整line1

--- strategy/line1.js
+++ strategy/line1.js
@@ -23,23 +23,24 @@ async function getLongOrShort(symbol) {
     let canLong = false
     let canShort = false
 
+    const ma0 = await binance.getKline(symbol, '1m', 2)
     const ma1 = await binance.getKline(symbol, '3m', 3) // 3min的kline 最近 n 条值
     const ma2 = await binance.getKline(symbol, '5m', 3) // 5min的kline 最近 n 条值
-    const ma3 = await binance.getKline(symbol, '30m', 3) // 30min的kline 最近 n 条值
+    // const ma3 = await binance.getKline(symbol, '10m', 3) // 30min的kline 最近 n 条值
     if (
+      isDesc(ma0) &&
       isDesc(ma1) &&
       isDesc(ma2) &&
-      kdj(ma1.slice(0, 2), ma2.slice(0, 2), 'long') &&
-      kdj(ma2.slice(0, 2), ma3.slice(0, 2), 'long')
+      kdj(ma1.slice(0, 2), ma2.slice(0, 2), 'long')
     ) { // 产生了金叉
       // 涨的时刻
       canLong = true
       canShort = false
     } else if (
+      isAsc(ma0) &&
       isAsc(ma1) &&
       isAsc(ma2) &&
-      kdj(ma1.slice(0, 2), ma2.slice(0, 2), 'short') &&
-      kdj(ma2.slice(0, 2), ma3.slice(0, 2), 'short')
+      kdj(ma1.slice(0, 2), ma2.slice(0, 2), 'short')
     ) {
       // 跌的时刻
       canLong = false
