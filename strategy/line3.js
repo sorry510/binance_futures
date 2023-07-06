c171e1d1234544db0db22730d0e95069760d1000
HaoZhengZhao <haozheng.zhao@poper.co>
2023-07-06 18:09:43 +0800

    fix: 调整 line3

--- strategy/line3.js
+++ strategy/line3.js
@@ -24,15 +24,19 @@ async function getLongOrShort(symbol) {
     let canShort = false
 
     const kline1m = await binance.getKline(symbol, '1m', 20) // 1min的kline 最近 n 条值
-    const kline15m = await binance.getKline(symbol, '15m', 21) // 3min的kline 最近 n 条值
-    const ma0 = maN(kline15m, 8)
-    const ma1 = maN(kline15m, 13)
-    const ma2 = maN(kline15m, 21)
+    const kline15m = await binance.getKline(symbol, '15m', 41) // 3min的kline 最近 n 条值
+    const { bids, asks } = await binance.depth(symbol, 50)
+    const buyCount = bids.reduce((carry, item) => Number(item[1]) + carry, 0) // 买单数量
+    const sellCount = asks.reduce((carry, item) => Number(item[1]) + carry, 0) // 卖单数量
+    const ma0 = maN(kline15m, 2)
+    const ma1 = maN(kline15m, 20)
+    const ma2 = maN(kline15m, 40)
     if (
       isDesc(kline1m.slice(0, 2)) &&
       isDesc(kline15m.slice(0, 2)) &&
       ma0 > ma1 &&
-      ma1 > ma2
+      ma1 > ma2 &&
+      buyCount > sellCount
     ) { // 产生了金叉
       // 涨的时刻
       canLong = true
@@ -41,7 +45,8 @@ async function getLongOrShort(symbol) {
       isAsc(kline1m.slice(0, 2)) &&
       isAsc(kline15m.slice(0, 2)) &&
       ma0 < ma1 &&
-      ma1 < ma2
+      ma1 < ma2 &&
+      buyCount < sellCount
     ) {
       // 跌的时刻
       canLong = false
